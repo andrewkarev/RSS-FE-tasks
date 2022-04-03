9a75710dc1a5c49004a72a2d77f31d9da160c6e1
andrewkarev <an.carev4@gmail.com>
2022-04-03 23:20:10 +0300

    feat: add functionality to the burger menu

--- shelter/pages/main/script.js
+++ shelter/pages/main/script.js
@@ -1,1 +1,27 @@
-// main page script.js
+// Burger menu
+const body = document.querySelector('body');
+const burgerMenu = document.querySelector('.burger-menu');
+const header = document.querySelector('.header');
+const headerContainer = document.querySelector('.header__container');
+const navigation = document.querySelector('.navigation');
+
+burgerMenu.addEventListener('click', () => {
+  body.classList.toggle('body--lock');
+  burgerMenu.classList.toggle('burger-menu--open');
+  header.classList.toggle('header--mobile');
+  headerContainer.classList.toggle('header__container--mobile');
+  navigation.classList.toggle('navigation--show');
+})
+
+document.addEventListener('click', (e) => {
+  const isHeaderElement = e.target.classList.contains('header--mobile');
+  const isNavigationItem = e.target.classList.contains('list__link');
+
+  if (isHeaderElement || isNavigationItem) {
+    body.classList.remove('body--lock');
+    burgerMenu.classList.remove('burger-menu--open');
+    header.classList.remove('header--mobile');
+    headerContainer.classList.remove('header__container--mobile');
+    navigation.classList.remove('navigation--show');
+  }
+})
