a1c92fee0cd258c97290ceaf463e2a86d0c289d1
andrewkarev <an.carev4@gmail.com>
2022-04-04 10:56:54 +0300

    fix: improved main page burger menu

--- shelter/pages/main/script.js
+++ shelter/pages/main/script.js
@@ -1,27 +1,27 @@
 // Burger menu
 const body = document.querySelector('body');
 const burgerMenu = document.querySelector('.burger-menu');
-const header = document.querySelector('.header');
 const headerContainer = document.querySelector('.header__container');
 const navigation = document.querySelector('.navigation');
+const background = document.querySelector('.background');
 
 burgerMenu.addEventListener('click', () => {
   body.classList.toggle('body--lock');
   burgerMenu.classList.toggle('burger-menu--open');
-  header.classList.toggle('header--mobile');
   headerContainer.classList.toggle('header__container--mobile');
   navigation.classList.toggle('navigation--show');
+  background.classList.toggle('background--blured');
 })
 
 document.addEventListener('click', (e) => {
-  const isHeaderElement = e.target.classList.contains('header--mobile');
+  const isBackgroundElement = e.target.classList.contains('background--blured');
   const isNavigationItem = e.target.classList.contains('list__link');
 
-  if (isHeaderElement || isNavigationItem) {
+  if (isBackgroundElement || isNavigationItem) {
     body.classList.remove('body--lock');
     burgerMenu.classList.remove('burger-menu--open');
-    header.classList.remove('header--mobile');
     headerContainer.classList.remove('header__container--mobile');
     navigation.classList.remove('navigation--show');
+    background.classList.toggle('background--blured');
   }
 })
