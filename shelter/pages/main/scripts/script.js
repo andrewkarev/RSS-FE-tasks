c1cfcc345dfefc64ccdad4be0511b76bf13c02b5
andrewkarev <an.carev4@gmail.com>
2022-04-06 19:52:11 +0300

    init: start shelter-js task

--- shelter/pages/main/scripts/script.js
+++ shelter/pages/main/scripts/script.js
@@ -1,4 +1,5 @@
 import { burgerMenu, toggleMenu, closeMenu } from './modules/burger-menu.js';
+import PETS from './modules/our-pets.js';
 
 document.addEventListener('click', closeMenu);
 burgerMenu.addEventListener('click', toggleMenu);
