c72fa094e5ce01ab032b8b3d320390e67202470b
andrewkarev <an.carev4@gmail.com>
2022-04-08 07:57:41 +0300

    feat: add popup appearance animation

--- shelter/pages/main/scripts/modules/popup.js
+++ shelter/pages/main/scripts/modules/popup.js
@@ -2,17 +2,20 @@ import { body, headerPetsPage } from './burger-menu.js';
 export const petsCard = document.querySelectorAll('.pets-card');
 export const popup = document.querySelector('.popup');
 export const popupCloseButton = document.querySelector('.popup__close-button');
+const popupContainer = document.querySelector('.popup__container');
 // const body = document.querySelector('body');
 
 export function showPopup() {
   body.classList.add('body--lock');
   popup.classList.add('popup--show');
+  popupContainer.classList.add('popup__container--show');
   checkPage()
 }
 
 export function closePopup() {
   body.classList.remove('body--lock');
   popup.classList.remove('popup--show');
+  popupContainer.classList.remove('popup__container--show');
   checkPage()
 }
 
