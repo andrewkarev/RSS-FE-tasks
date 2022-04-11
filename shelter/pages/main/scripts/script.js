5910477ce27d538c52b6e0afa0441c458b3e7d34
andrewkarev <an.carev4@gmail.com>
2022-04-11 20:29:38 +0300

    feat: add dynamic pagination step update

--- shelter/pages/main/scripts/script.js
+++ shelter/pages/main/scripts/script.js
@@ -36,12 +36,20 @@ const paginationButtonNext = document.querySelector('.button-arrow--next-page');
 const paginationButtonPrevious = document.querySelector('.button-arrow--previous-page');
 const paginationPagePointer = document.querySelector('.page-pointer__inner');
 const paginationElements = [];
-const step = 8;
+let step = 8;
 const paginationElementsCounter = 48;
-const pagesAtAll = paginationElementsCounter / step;
+let pagesAtAll;
 let elementsOnPage = 0;
 let isNextButtonsActive = true;
 let isPreviousButtonsActive = true;
+
+
+// Calculate current step
+function getStep() {
+  const getScreebWidth = document.documentElement.clientWidth;
+  return getScreebWidth < 768 ? 3 : getScreebWidth <= 1280 ? 6 : 8;
+}
+
 
 // Create initial pets page pagination elements list
 function fillPaginationElements() {
@@ -57,6 +65,9 @@ function fillPaginationElements() {
 
 // Draw pets cards
 function renderCards() {
+  step = getStep();
+  pagesAtAll = paginationElementsCounter / step;
+
   petsCardsOurFriends.innerHTML = '';
   for (let i = elementsOnPage; i < elementsOnPage + step; i++) {
     const card = createCardInner.call(paginationElements[i], 'pets-card--our-friends');
@@ -67,7 +78,7 @@ function renderCards() {
 }
 
 function showNextPage() {
-  elementsOnPage += 8;
+  elementsOnPage += step;
 
   const currentPage = +paginationPagePointer.innerHTML + 1
   paginationPagePointer.innerHTML = `${currentPage}`;
@@ -78,7 +89,7 @@ function showNextPage() {
 
 
 function showLastPage() {
-  elementsOnPage = 40;
+  elementsOnPage = paginationElementsCounter - step;
 
   paginationPagePointer.innerHTML = `${pagesAtAll}`;
 
@@ -87,7 +98,7 @@ function showLastPage() {
 }
 
 function showPreviousPage() {
-  elementsOnPage -= 8;
+  elementsOnPage -= step;
 
   const currentPage = +paginationPagePointer.innerHTML - 1
   paginationPagePointer.innerHTML = `${currentPage}`;
@@ -109,7 +120,7 @@ function showFirstPage() {
 // Change buttons style and handler status
 function changePaginationButtonsState(arg) {
   if (arg) {
-    if (elementsOnPage === paginationElements.length - 8) {
+    if (elementsOnPage === paginationElementsCounter - step) {
       isNextButtonsActive = false;
       paginationButtonLast.classList.add('button-arrow--disabled');
       paginationButtonNext.classList.add('button-arrow--disabled');
@@ -161,3 +172,6 @@ paginationButtonNext.addEventListener('click', showNextPage);
 paginationButtonLast.addEventListener('click', showLastPage);
 paginationButtonPrevious.addEventListener('click', showPreviousPage);
 paginationButtonFirst.addEventListener('click', showFirstPage);
+
+// Should pagination be able to change in lifetime?
+window.addEventListener('resize', showFirstPage);
