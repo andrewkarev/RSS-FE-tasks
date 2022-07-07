9ffac3daeec0cc0a41e1293254a74255af46b86a
Andrew <an.carev4@gmail.com>
2022-07-07 20:20:07 +0300

    feat: implement function for element creation

---
+++ online-store/src/components/utils/create-el.ts
@@ -0,0 +1,55 @@
+import CreateElement from './types/create-el-func';
+import Attributes from './types/attributes';
+
+const createEl: CreateElement = (
+  tagName: string,
+
+  classNames?: string[] | string,
+
+  innerContent?: HTMLElement[] | string | CreateElement,
+
+  parentNode?: HTMLElement,
+
+  ...attributes: Attributes[]
+
+): HTMLElement => {
+  const element: HTMLElement = document.createElement(tagName);
+
+  if (classNames) {
+    if (Array.isArray(classNames)) {
+      classNames.forEach((className) => element.classList.add(className));
+    } else if (typeof classNames === 'string') {
+      element.classList.add(`${classNames}`);
+    }
+  }
+
+  if (innerContent) {
+    if (Array.isArray(innerContent)) {
+      innerContent.forEach((child) => element.append(child));
+    } else if (typeof innerContent === 'object') {
+      element.append(innerContent);
+    } else if (typeof innerContent === 'string') {
+      element.innerHTML = innerContent;
+    }
+  }
+
+  if (attributes) {
+    attributes.forEach(([attrName, attrValue]) => {
+      if (attrValue) {
+        if (attrName === 'serialNum') {
+          element.dataset[attrName] = `${attrValue}`;
+        } else {
+          element.setAttribute(attrName, `${attrValue}`);
+        }
+      } else {
+        element.setAttribute(attrName, '');
+      }
+    });
+  }
+
+  if (parentNode) parentNode.append(element);
+
+  return element;
+};
+
+export default createEl;
