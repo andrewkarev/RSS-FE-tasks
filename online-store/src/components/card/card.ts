d3164967c69d3bf8a79d6aea4e72b10e0a4d6935
Andrew <an.carev4@gmail.com>
2022-07-07 20:20:55 +0300

    feat: implement product card class

---
+++ online-store/src/components/card/card.ts
@@ -0,0 +1,58 @@
+import createEl from '../utils/create-el';
+import ICard from '../utils/interfaces/ICard';
+
+export default class Card {
+  brand: string;
+
+  model: string;
+
+  year: string;
+
+  color: string;
+
+  storage: string;
+
+  price: number;
+
+  popular: boolean;
+
+  img: string;
+
+  serialNum: number;
+
+  div?: Node;
+
+  constructor(
+    {
+      brand, model, year, color, storage, price, popular, img, serialNum,
+    }: ICard,
+  ) {
+    this.brand = brand;
+    this.model = model;
+    this.year = year;
+    this.color = color;
+    this.storage = storage;
+    this.price = price;
+    this.popular = popular;
+    this.img = img;
+    this.serialNum = serialNum;
+  }
+
+  create() {
+    const cardInner = `
+      <h2 class="goods__title">${this.model}</h2>
+      <figure class="goods__item">
+        <img class="goods__img" src="${this.img}" alt="${this.model}">
+        <figcaption class="goods__caption">
+          ${this.brand} / ${this.year} / ${this.color} / ${this.storage}GB<br>
+          $${this.price} / ${this.popular ? 'popular' : 'not so popular'}
+        </figcaption>
+      </figure>
+      <div class="btn-container">
+        <button class=" goods__save-button button" type="button">Save</button>
+        <button class="goods__buy-button button" type="button">Buy</button>
+      </div>`;
+
+    this.div = createEl('div', 'goods__card', cardInner, undefined, ['serialNum', +this.serialNum]);
+  }
+}
