3e04e00b00332ce1aaa072af0f1e614258e7a233
Andrew <an.carev4@gmail.com>
2022-07-07 20:14:49 +0300

    feat: implement product card interface

---
+++ online-store/src/components/utils/interfaces/ICard.ts
@@ -0,0 +1,13 @@
+interface ICard {
+  brand: string;
+  model: string;
+  year: string;
+  color: string;
+  storage: string;
+  price: number;
+  popular: boolean;
+  img: string;
+  serialNum: number;
+}
+
+export default ICard;
