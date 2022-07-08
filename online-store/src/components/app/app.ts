f5d530ac35fb591fd70cdaa2cee9a1a6a0faf0e4
Andrew <an.carev4@gmail.com>
2022-07-08 11:23:05 +0300

    feat: implement cards sorting function

--- online-store/src/components/app/app.ts
+++ online-store/src/components/app/app.ts
@@ -2,6 +2,7 @@
 import Card from '../card/card';
 import goods from '../data/goods-db';
 import AppMenu from '../appMenu/appMenu';
+import ICard from '../utils/interfaces/ICard';
 
 class App {
   cards: Card[];
@@ -16,10 +17,17 @@ class App {
     this.menu = new AppMenu(0, 0);
   }
 
-  generateCards(): void {
+  init(sortingOrder: string) {
+    const relevantGoods: ICard[] = goods;
+
+    const chosenGoods = App.sort(relevantGoods, sortingOrder);
+    this.generateCards(chosenGoods);
+  }
+
+  generateCards(chosenGoods: ICard[]): void {
     this.cardsContainer = document.querySelector('.goods') as Node;
 
-    goods.forEach((item) => {
+    chosenGoods.forEach((item) => {
       const card = new Card(item);
       card.create();
 
@@ -65,6 +73,48 @@ class App {
         }
       }
     });
+  }
+
+  static sort(relevantGoods: ICard[], sortingOrder: string) {
+    if (sortingOrder === 'oldest') {
+      relevantGoods.sort((a, b) => a.year - b.year);
+    }
+
+    if (sortingOrder === 'newest') {
+      relevantGoods.sort((a, b) => b.year - a.year);
+    }
+
+    if (sortingOrder === 'highest') {
+      relevantGoods.sort((a, b) => b.price - a.price);
+    }
+
+    if (sortingOrder === 'lowest') {
+      relevantGoods.sort((a, b) => a.price - b.price);
+    }
+
+    if (sortingOrder === 'name-increasing') {
+      relevantGoods.sort((a, b) => {
+        const nameA: string = a.model.toLowerCase();
+        const nameB: string = b.model.toLowerCase();
+
+        if (nameA < nameB) return -1;
+        if (nameA > nameB) return 1;
+        return 0;
+      });
+    }
+
+    if (sortingOrder === 'name-decreasing') {
+      relevantGoods.sort((a, b) => {
+        const nameA: string = a.model.toLowerCase();
+        const nameB: string = b.model.toLowerCase();
+
+        if (nameA > nameB) return -1;
+        if (nameA < nameB) return 1;
+        return 0;
+      });
+    }
+
+    return relevantGoods;
   }
 }
 
--- online-store/src/index.ts
+++ online-store/src/index.ts
@@ -3,4 +3,4 @@ import App from './components/app/app';
 
 const app = new App();
 
-app.generateCards();
+app.init('name-decreasing');
