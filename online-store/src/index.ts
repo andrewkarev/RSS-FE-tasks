e1666c200dc914c28feb60921a7be71295727365
Andrew <an.carev4@gmail.com>
2022-07-09 16:01:57 +0300

    feat: update index.ts

--- online-store/src/index.ts
+++ online-store/src/index.ts
@@ -1,6 +1,14 @@
 import './style/main.scss';
 import App from './components/app/app';
+import { get } from './components/utils/storage';
+import IMenuItems from './components/utils/interfaces/iMenuItems';
+
+const sortingOrder: string = get('sortingOption', '"ascending"');
+const menuItems: IMenuItems = {
+  savedCards: get('savedItems', '[]'),
+  cardsInCart: get('shoppingCart', '[]'),
+};
 
 const app = new App();
 
-app.init('name-decreasing');
+app.init(sortingOrder, menuItems);
