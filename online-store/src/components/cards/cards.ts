3df2d898ddcf7018da53a1b3cb9626a072c2add7
Andrew <an.carev4@gmail.com>
2022-07-10 19:17:13 +0300

    fix: delete useless field from `Cards` class

--- online-store/src/components/cards/cards.ts
+++ online-store/src/components/cards/cards.ts
@@ -2,13 +2,7 @@ import Card from '../card/card';
 import ICard from '../utils/interfaces/ICard';
 
 class Cards {
-  renderedCards: Card[];
-
   cardsContainer?: HTMLElement;
-
-  constructor() {
-    this.renderedCards = [];
-  }
 
   generateCards(chosenGoods: ICard[]): void {
     this.cardsContainer = document.querySelector('.goods') as HTMLElement;
@@ -18,8 +12,6 @@ class Cards {
     chosenGoods.forEach((item) => {
       const card = new Card(item);
       card.create();
-
-      this.renderedCards.push(card);
 
       if (card.div) {
         this.cardsContainer?.appendChild(card.div);
