eccf0b4e3f6675bf0aa5fed8a41325780ac5bf02
Andrew <an.carev4@gmail.com>
2022-07-11 18:53:52 +0300

    refactor: split filter elements into separate classes

--- online-store/src/components/filters/filters.ts
+++ online-store/src/components/filters/filters.ts
@@ -1,90 +1,29 @@
-import filtersElements from '../data/filters-elements';
-import createEl from '../utils/create-el';
+import BrandFilter from './brandFilter';
+import ColorFilter from './colorFilter';
+import StorageFilter from './storageFilter';
+import PopularityFilter from './popularityFilter';
 
 class Filters {
-  brandFilter?: HTMLElement;
+  brandFilter: BrandFilter;
 
-  colorFilter?: HTMLElement;
+  colorFilter: ColorFilter;
 
-  storageFilter?: HTMLElement;
+  storageFilter: StorageFilter;
 
-  popularityFilter?: HTMLElement;
+  popularityFilter: PopularityFilter;
 
-  generateFilters(): void {
-    this.generateBrandFilter();
-    this.generateColorFilter();
-    this.generateStorageSizeFilter();
-    this.generatePopularityFilter();
+  constructor() {
+    this.brandFilter = new BrandFilter();
+    this.colorFilter = new ColorFilter();
+    this.storageFilter = new StorageFilter();
+    this.popularityFilter = new PopularityFilter();
   }
 
-  generateBrandFilter(): void {
-    const currentFilter = filtersElements.brandFilter;
-
-    this.brandFilter = document.querySelector(currentFilter.parentNodeName) as HTMLElement;
-
-    currentFilter.elements.forEach((brand) => {
-      createEl(
-        'button',
-        currentFilter.classNames,
-        `${currentFilter.pointer} ${brand}`,
-        this.brandFilter,
-        [currentFilter.attributes[0], currentFilter.attributes[1]],
-        [currentFilter.name, brand],
-      );
-    });
-  }
-
-  generateColorFilter(): void {
-    const currentFilter = filtersElements.colorFilter;
-
-    this.colorFilter = document.querySelector(currentFilter.parentNodeName) as HTMLElement;
-
-    currentFilter.elements.forEach((color, idx) => {
-      const currentClassNames = currentFilter.classNames.slice(0, 2);
-      currentClassNames.push(currentFilter.classNames[idx + 2]);
-
-      createEl(
-        'button',
-        currentClassNames,
-        undefined,
-        this.colorFilter,
-        [currentFilter.attributes[0], currentFilter.attributes[1]],
-        [currentFilter.name, color],
-      );
-    });
-  }
-
-  generateStorageSizeFilter(): void {
-    const currentFilter = filtersElements.storageFilter;
-
-    this.storageFilter = document.querySelector(currentFilter.parentNodeName) as HTMLElement;
-
-    currentFilter.elements.forEach((storageSize) => {
-      createEl(
-        'button',
-        currentFilter.classNames,
-        `${currentFilter.pointer} ${storageSize} GB`,
-        this.storageFilter,
-        [currentFilter.attributes[0], currentFilter.attributes[1]],
-        [currentFilter.name, storageSize],
-      );
-    });
-  }
-
-  generatePopularityFilter(): void {
-    const currentFilter = filtersElements.popularityFilter;
-
-    this.popularityFilter = document.querySelector(currentFilter.parentNodeName) as HTMLElement;
-
-    currentFilter.elements.forEach((item) => {
-      createEl(
-        'button',
-        currentFilter.classNames,
-        `${currentFilter.pointer} ${item}`,
-        this.popularityFilter,
-        [currentFilter.attributes[0], currentFilter.attributes[1]],
-      );
-    });
+  generateFilters(brandfilterOptions: string[]): void {
+    this.brandFilter.generateFilter(brandfilterOptions);
+    this.colorFilter.generateFilter();
+    this.storageFilter.generateFilter();
+    this.popularityFilter.generateFilter();
   }
 }
 
