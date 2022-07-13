90a4de889576ed38e5fa9ea1140abff7373c6312
Andrew <an.carev4@gmail.com>
2022-07-13 19:12:09 +0300

    feat: implement class for quantity filter

---
+++ online-store/src/components/filters/quantityFilter.ts
@@ -0,0 +1,63 @@
+import * as noUiSlider from 'nouislider';
+import 'nouislider/dist/nouislider.css';
+import ICard from '../utils/interfaces/ICard';
+import { set } from '../utils/storage';
+import DragFilter from '../utils/abstract-classes/dragFilterClass';
+
+class QuantityFilter extends DragFilter {
+  quantitySlider?: noUiSlider.target;
+
+  relevantValue?: string[];
+
+  generateFilter([from, to]: string[]): void {
+    this.quantitySlider = document.getElementById('quantity-slider') as noUiSlider.target;
+
+    this.relevantValue = [from, to];
+
+    if (this.quantitySlider) {
+      noUiSlider.create(this.quantitySlider, {
+        start: [from, to],
+        step: 1,
+        tooltips: true,
+        behaviour: 'drag',
+        connect: true,
+        range: {
+          min: 1,
+          max: 19,
+        },
+        format: {
+          from(value) {
+            return parseInt(value, 10);
+          },
+          to(value) {
+            return Number(value).toFixed();
+          },
+        },
+      });
+    }
+  }
+
+  handleDrag(): void {
+    const currentValues = this.quantitySlider?.noUiSlider?.get() as string[];
+    this.relevantValue = currentValues;
+    set('quantityFilter', this.relevantValue);
+  }
+
+  filterData(relevantGoods: ICard[]): ICard[] {
+    let filtredGoods: ICard[] = relevantGoods;
+    let fromQuantity: number;
+    let toQuantity: number;
+
+    if (this.relevantValue) {
+      fromQuantity = parseInt(this.relevantValue[0], 10);
+      toQuantity = parseInt(this.relevantValue[1], 10);
+    }
+
+    filtredGoods = relevantGoods
+      .filter((item) => item.quantity >= fromQuantity && item.quantity <= toQuantity);
+
+    return filtredGoods;
+  }
+}
+
+export default QuantityFilter;
