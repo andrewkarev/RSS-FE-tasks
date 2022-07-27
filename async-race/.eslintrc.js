cb5638a7d91facb3f4903844bd52e2995dfa47f5
Andrew <91889887+andrewkarev@users.noreply.github.com>
2022-07-27 12:57:23 +0300

    init: start async race task

---
+++ async-race/.eslintrc.js
@@ -0,0 +1,72 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'airbnb-base',
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/recommended-requiring-type-checking'
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    project: 'tsconfig.json',
+    tsconfigRootDir: __dirname,
+    ecmaVersion: 'latest',
+    sourceType: 'module'
+  },
+  plugins: [
+    '@typescript-eslint'
+  ],
+  rules: {
+    "no-useless-constructor": "off",
+    "@typescript-eslint/no-useless-constructor": ["error"],
+    'no-shadow': 'off',
+    '@typescript-eslint/no-shadow': [
+      'error'
+    ],
+    'no-unused-expressions': [
+      'error',
+      {
+        "allowTernary": true
+      }
+    ],
+    '@typescript-eslint/no-explicit-any': 'error',
+    '@typescript-eslint/array-type': [
+      'error',
+      {
+        'default': 'array',
+        'readonly?': 'array'
+      }
+    ],
+    'import/extensions': [
+      'error',
+      'ignorePackages',
+      {
+        'js': 'never',
+        'jsx': 'never',
+        'ts': 'never',
+        'tsx': 'never'
+      }
+    ]
+  },
+  settings: {
+    'import/resolver': {
+      'node': {
+        'extensions': [
+          '.js',
+          '.jsx',
+          '.ts',
+          '.tsx'
+        ]
+      }
+    }
+  },
+  ignorePatterns: [
+    'webpack.config.js',
+    '.eslintrc.js',
+    'jest.config.js',
+  ],
+  'root': true,
+}
