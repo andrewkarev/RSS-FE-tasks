1b803e0216dd5e4ed0f5e58b51155594c456641d
Andrew <91889887+andrewkarev@users.noreply.github.com>
2022-08-01 19:05:58 +0300

    feat: implement function for getting random color

--- async-race/src/components/utils.ts
+++ async-race/src/components/utils.ts
@@ -1,9 +1,19 @@
 import { renderWinningMessage } from './UI';
 
-const showWinningMessage = () => {
+export const showWinningMessage = () => {
   const root = document.getElementById('body');
   const message = renderWinningMessage('BMW', 2.2);
   root?.append(message);
 };
 
-export default showWinningMessage;
+export const getRandomColor = (): string => {
+  const hexadecimal = '0123456789ABCDEF';
+  let color = '#';
+
+  for (let i = 0; i < 6; i += 1) {
+    const randomIndex = Math.floor(Math.random() * hexadecimal.length);
+    color += hexadecimal[randomIndex];
+  }
+
+  return color;
+};
