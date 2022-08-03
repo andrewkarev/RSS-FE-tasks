7d76ae7eeb535bffbcf71bc8e4e69ed020a97a26
Andrew <91889887+andrewkarev@users.noreply.github.com>
2022-08-03 10:48:12 +0300

    feat: implement an object with app state

---
+++ async-race/src/components/appState.ts
@@ -0,0 +1,28 @@
+const state = {
+  carsBrands: [
+    'Tesla',
+    'Aston Martin',
+    'Rolls-Royce',
+    'Ferrari',
+    'Lamborghini',
+    'Porsche',
+    'Maserati',
+    'Ford',
+    'Mercedes',
+    'Audi',
+  ],
+  carsModels: [
+    'Model S',
+    'DBS',
+    'Phantom (VII)',
+    'Roma',
+    'huracan',
+    'Carrera GT',
+    'GranTurismo',
+    'Mustang Shelby GT 500',
+    'AMG GT',
+    'R8',
+  ],
+};
+
+export default state;
