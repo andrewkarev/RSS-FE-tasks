03e4fe2dc4fd36b46fb11057aad9c3e9da7fd049
Andrew <an.carev4@gmail.com>
2022-08-06 20:23:25 +0300

    feat: implement function for getting race winner

--- async-race/src/components/utils.ts
+++ async-race/src/components/utils.ts
@@ -5,10 +5,11 @@ const getRandomNumber = (
   item: string | string[],
 ): number => Math.floor(Math.random() * item.length);
 
-export const showWinningMessage = (name: string, time: number): void => {
+const showWinningMessage = (name: string, time: number): HTMLElement => {
   const root = document.getElementById('body');
   const message = renderWinningMessage(name, time);
   root?.append(message);
+  return message;
 };
 
 export const getRandomColor = (): string => {
@@ -27,4 +28,17 @@ export const getRandomCarName = (): string => {
   const brandOption = state.carsBrands[getRandomNumber(state.carsBrands)];
   const modelOption = state.carsModels[getRandomNumber(state.carsModels)];
   return `${brandOption} ${modelOption}`;
+};
+
+export const handleAnimationEnd = (id: number, duration: number) => {
+  if (!state.raceWinnerId && state.isRace) {
+    state.raceWinnerId = id;
+
+    const winnerCar = document.getElementById(`car-name-${id}`);
+    const winnerTime = +(duration / 1000).toFixed(2);
+    const winnreName = winnerCar?.innerText || '';
+
+    const message = showWinningMessage(winnreName, winnerTime);
+    window.addEventListener('click', () => message.remove());
+  }
 };
